5ea7a911c38bdf1905d48f38cf855eb47f4a0661
Aung Myint Myat <amm926616@gmail.com>
2025-05-03 02:16:07 +0630

    added git background

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # image-hosting
+
+### React + Tailwind (frontend) + Django backend to serve images
+
+![project overview](./github_background/project_overview0.png)
+
+![project overview](./github_background/project_overview1.png)
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,14 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = '/images/' #
-MEDIA_ROOT = "/home/aiden178/Pictures/.kkscrape/"
+MEDIA_ROOT = "/home/aiden178/Pictures/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "backend", "media")
 
 # Quick-start development settings - unsuitable for production
